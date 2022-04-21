1a611801d172ad28e5d6c7c4d7a2f5fa89b8a137
huangxi <huangxi@alnmp.com>
2022-04-21 11:42:50 +0800

    init

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hello;
+
+/**
+ *
+ * Author: huangxi <huangxi@alnmp.com>
+ * Date: 2022/4/21
+ */
+class Test
+{
+    function say()
+    {
+        echo 'say';
+    }
+}
